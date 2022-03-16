6d04c3ecd94251caddf5c4ff660e828f35eac5e9
huhongyue <huhongyue@atyun.net>
2022-03-16 21:48:08 +0800

    ios黑屏修改

--- lib/src/core/better_player_controller.dart
+++ lib/src/core/better_player_controller.dart
@@ -564,7 +564,7 @@ class BetterPlayerController {
         if (_appLifecycleState == AppLifecycleState.resumed &&
             _isPlayerVisible) {
           if (Platform.isIOS) {
-            Future.delayed(Duration(milliseconds: 500), () async {
+            Future.delayed(Duration(milliseconds: 1500), () async {
               await play();
             });
           } else {
